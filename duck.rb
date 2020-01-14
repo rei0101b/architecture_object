4b43dcc06c6b6a40351da39e8858267010cd22dd
rei0101b <rei0101b@gmail.com>
2020-01-14 11:51:22 +0900

    duck type

---
+++ duck.rb
@@ -0,0 +1,40 @@
+class Trip
+  attr_reader :bicycles, :customers, :vehicle
+
+  def prepare(preparers)
+    preparers.each do |preparer|
+      preparer.prepare_trip(self)
+    end
+  end
+end
+
+class TripCoordinator
+  def prepare_trip(trip)
+    buy_food(trip.customers)
+  end
+end
+
+class Driver
+
+  def prepare_trip(trip)
+    vehicle = trip.vehicle
+    gas_up(vehicle)
+    fill_water_tank(vehicle)
+  end
+  
+  def gas_up(vehicle)
+
+  end
+
+  def fill_water_tank(vehicle)
+
+  end
+end
+
+class Mechanic
+  def prepare_trip(trip)
+    trip.bicycles.each do |bicycle|
+      prepare_bicycle(bicycle)
+    end
+  end
+end
